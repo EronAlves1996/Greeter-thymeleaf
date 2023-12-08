61342d621b3f9aa75e8d464afbe545f581f2479d
EronAlves1996 <eron.alves@rocketmail.com>
2023-12-07 22:16:45 -0300

    initial commit

---
+++ src/test/java/com/eronalves/greeter/GreeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eronalves.greeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
